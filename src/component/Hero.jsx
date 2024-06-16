952cb277edbca44ac93bcf9d6ff7ad563b181640
jeevan07854 <jeevan1.785401@cct.tu.edu.np>
2024-06-16 22:58:32 +0545

    update Hero.jsx and Hero.css

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,28 +1,22 @@
-import React from 'react'
+import React from "react"
 import "./Hero.css"
 const Hero = () => {
   return (
     <>
+      <div className="hero-wrapper">
+        <div className="hero-left">
+          <h3>
+            Organic produce is an excellent choice for those seeking fresh,
+            flavorful, and eco-friendly food. Catchy slogans can be used to
+            highlight the advantages of selecting organic fruits and vegetables
+            and make a lasting impact on potential customers.
+          </h3>
 
+          <h2>My name is Jeevan Poudel and i am from itahari</h2>
+        </div>
 
-
-    <div className="hero-wrapper">
-<div className="hero-left">
-<h3>Organic produce is an excellent choice for those seeking fresh, flavorful, and eco-friendly food. Catchy slogans can be used to highlight the advantages of selecting organic fruits and vegetables and make a lasting impact on potential customers.</h3>
-
- <h2>My name is Jeevan Poudel</h2>
-
-</div>
- 
- <div className="hero-right">
-
-
-
- </div>
-    </div>
-    
-    
-    
+        <div className="hero-right"></div>
+      </div>
     </>
   )
 }
